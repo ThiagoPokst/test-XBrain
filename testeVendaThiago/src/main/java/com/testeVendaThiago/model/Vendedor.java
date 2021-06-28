c1e48d53f47177817b0b15418ad7f18d91d1a81c
ThiagoPokst <31635628+ThiagoPokst@users.noreply.github.com>
2021-06-28 10:50:06 -0300

    Add files via upload

--- testeVendaThiago/src/main/java/com/testeVendaThiago/model/Vendedor.java
+++ testeVendaThiago/src/main/java/com/testeVendaThiago/model/Vendedor.java
@@ -1,14 +1,73 @@
 package com.testeVendaThiago.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.ToString;
+
 
 @Entity
 @Table(name = "Vendedor")
-@ToString
-
 public class Vendedor {
-	@Name
+	@Id
+	@Column(name = "vendedorId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idVendedor;
+	@Column(name = "nomeVendedor",nullable = false)
+	private String nomeVendedor;
+	
+	public Vendedor() {
+	}
+
+	public Vendedor(Long idVendedor, String nomeVendedor) {
+		super();
+		this.idVendedor = idVendedor;
+		this.nomeVendedor = nomeVendedor;
+	}
+
+	public Long getIdVendedor() {
+		return idVendedor;
+	}
+
+	public void setIdVendedor(Long idVendedor) {
+		this.idVendedor = idVendedor;
+	}
+
+	public String getNomeVendedor() {
+		return nomeVendedor;
+	}
+
+	public void setNomeVendedor(String nomeVendedor) {
+		this.nomeVendedor = nomeVendedor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idVendedor == null) ? 0 : idVendedor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedor other = (Vendedor) obj;
+		if (idVendedor == null) {
+			if (other.idVendedor != null)
+				return false;
+		} else if (!idVendedor.equals(other.idVendedor))
+			return false;
+		return true;
+	}
+	
+	
 }
