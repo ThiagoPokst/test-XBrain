f1649b1e1e6d9f6ee2dff737d6fa0906eb1bdffa
ThiagoPokst <31635628+ThiagoPokst@users.noreply.github.com>
2021-06-28 13:26:32 -0300

    Add files via upload

--- testeVendaThiago/src/main/java/com/testeVendaThiago/controller/VendaController.java
+++ testeVendaThiago/src/main/java/com/testeVendaThiago/controller/VendaController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.testeVendaThiago.model.Venda;
+import com.testeVendaThiago.service.VendaService;
+
 @RestController
 @RequestMapping("/vendas")
 public class VendaController{
